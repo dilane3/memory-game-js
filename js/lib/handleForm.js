166408099f036c0d19a9fd60f1367d31a13add86
dilane3 <komboudilane125@gmail.com>
2021-02-22 16:16:40 +0100

    mise sur pied d'un etat local pour stoquer en permanance le nom et le score des deux joueurs

--- js/lib/handleForm.js
+++ js/lib/handleForm.js
@@ -2,10 +2,25 @@ let formInput = document.getElementById("form-input");
 let submitBtn = document.getElementById("btn-submit");
 let form = document.querySelector('.formular');
 let gameInterface = document.querySelector(".game-body");
+let gameHeader = document.querySelector(".game-header");
 let startBtn = document.querySelector(".start-button");
+let playerName = document.getElementById("js-player-name");
+let playerScore = document.getElementById("js-player-score");
+let cpuScore = document.getElementById("js-cpu-score");
 
 submitBtn.addEventListener('click', function() {
   fade(form, startBtn);
+  gameHeader.classList.add("fade-in");
+
+  localStorage.setItem("playerName", formInput.value);
+  localStorage.setItem("playerScore", 0);
+  localStorage.setItem("cpuScore", 0);
+
+  playerName.innerText = localStorage.getItem("playerName");
+  playerScore.innerText = localStorage.getItem("playerScore");
+  cpuScore.innerText = localStorage.getItem("cpuScore");
+
+  formInput.value = "";
 });
 
 startBtn.addEventListener('click', function () {
