c2e4c6ec89fd8d6e23a9f31d797da822985a16db
dilane3 <komboudilane125@gmail.com>
2021-02-22 21:48:13 +0100

    separation du design du formulaire dans un fichier form.css, et ajout de l'interactivite sur ce formulaire

--- js/lib/handleForm.js
+++ js/lib/handleForm.js
@@ -7,34 +7,66 @@ let startBtn = document.querySelector(".start-button");
 let playerName = document.getElementById("js-player-name");
 let playerScore = document.getElementById("js-player-score");
 let cpuScore = document.getElementById("js-cpu-score");
+let error = document.getElementById("error");
+let closeErrorAlert = document.querySelector(".close-alert");
 
 submitBtn.addEventListener('click', function() {
-  fade(form, startBtn);
-  gameHeader.classList.add("fade-in");
+  let name = formInput.value;
 
-  localStorage.setItem("playerName", formInput.value);
-  localStorage.setItem("playerScore", 0);
-  localStorage.setItem("cpuScore", 0);
+  if (name.length !== 0) {
+    if (name.length > 13) {
+      name = name.substr(0, 10) + "...";
+    }
 
-  playerName.innerText = localStorage.getItem("playerName");
-  playerScore.innerText = localStorage.getItem("playerScore");
-  cpuScore.innerText = localStorage.getItem("cpuScore");
+    fade(form, startBtn);
+    gameHeader.classList.add("fade-in");
+    console.log(gameHeader);
 
-  formInput.value = "";
+    localStorage.setItem("playerName", name);
+    localStorage.setItem("playerScore", 0);
+    localStorage.setItem("cpuScore", 0);
+
+    playerName.innerText = localStorage.getItem("playerName");
+    playerScore.innerText = localStorage.getItem("playerScore");
+    cpuScore.innerText = localStorage.getItem("cpuScore");
+
+    formInput.value = "";
+  } else {
+    error.classList.remove('d-none');
+    error.classList.add('visible');
+    error.style.opacity = 1;
+    
+  }
+});
+
+closeErrorAlert.addEventListener('click', function() {
+  fade(error);
+
+  let id = setTimeout(() => {
+    error.classList.add("d-none");
+    error.classList.remove("visible");
+
+    clearInterval(id);
+  }, 200);
 });
 
 startBtn.addEventListener('click', function () {
-  fade(startBtn);
-  hiddenElement(form);
-  hiddenElement(startBtn);
-  showElement(gameInterface);
-  gameInterface.classList.add("fade-in");
+  console.log(startBtn.classList.contains("hidden"));
+  if (!startBtn.classList.contains("hidden")) {
+    fade(startBtn);
+    hiddenElement(form);
+    hiddenElement(startBtn);
+    showElement(gameInterface);
+    gameInterface.classList.add("fade-in");
+  }
 });
 
 function fade(element, btn = null) {
   element.classList.add('fade-out');
+  element.classList.add('d-none');
 
   if (btn !== null) {
+    btn.classList.remove("hidden");
     let id = setTimeout(() => {
       btn.classList.add('fade-in-60');
 
