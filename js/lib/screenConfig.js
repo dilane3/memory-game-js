df942f2df3087303d96577b929e88393558aa9ea
dilane3 <komboudilane125@gmail.com>
2021-02-22 12:52:50 +0100

    creation of the interface of the memory game

---
+++ js/lib/screenConfig.js
@@ -0,0 +1,15 @@
+let game_body = document.getElementById("game-body");
+
+if (window.innerWidth > 500) {
+  game_body.style.height = `${window.innerHeight - 130}px`;
+} else {
+  game_body.style.height = `${window.innerHeight - 80}px`;
+}
+
+window.onresize = function() {
+  if (window.innerWidth > 500) {
+    game_body.style.height = `${window.innerHeight - 130}px`;
+  } else {
+    game_body.style.height = `${window.innerHeight - 80}px`;
+  }
+}
